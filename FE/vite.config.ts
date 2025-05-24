3601ef16bb85b68b206dbc47803f92d818feeb18
Eric <ssicose@gmail.com>
2025-05-24 16:06:27 +1200

    another attempt

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import fs from 'fs'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 const isDev = process.env.NODE_ENV === 'development'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      {
+        src: 'staticwebapp.config.json',
+        dest: '.' // copy to dist/
+      }
+    ]
+  })],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
