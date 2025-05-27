ba97088e307c544e6df5f1723a145b39149d0419
Eric <ssicose@gmail.com>
2025-05-27 19:27:51 +1200

    add secrets to github action

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -14,6 +14,8 @@ const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
+console.log('domain', domain)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
